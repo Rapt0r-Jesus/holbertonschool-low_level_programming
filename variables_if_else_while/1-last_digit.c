96ae4ac9a1092755b23a1bef881a8ffb7e15ed19
root <dorian.oufer@gmail.com>
2025-06-18 11:57:53 +0000

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,23 +13,24 @@
 
 int main(void)
 {
-	int n, d;
+	int n;
+	int d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	d = n % 10;
 
 	if (n > 5)
-
-		printf("The last digit of the number %d is %d and is greater than 5\n", n, d);
-
+	{
+	printf("The last digit of the number %d is %d and is greater than 5\n", n, d);
+	}
 	else if (n == 0)
-
-		printf("The last digit of the number %d is %d and is 0\n", n, d);
-
+	{
+	printf("The last digit of the number %d is %d and is 0\n", n, d);
+	}
 	else
-
-		printf("The last digit of the number %d is %d and is less than 6 and not 0\n", n, d);
-
+	{
+	printf("The last digit of the number %d is %d and is less than 6 and not 0\n", n, d);
+	}
 	return (0);
 }
---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0; nbr < 10; nbr++)
+        {
+	putchar(nbr + '0');
+        }
+
+	putchar('\n');
+	return (0);
+}
