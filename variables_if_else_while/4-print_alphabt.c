70cbf678cfc13e7a291f689d307380e11e541770
root <dorian.oufer@gmail.com>
2025-06-18 11:23:42 +0000

    4-print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, then uppercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alpha;
