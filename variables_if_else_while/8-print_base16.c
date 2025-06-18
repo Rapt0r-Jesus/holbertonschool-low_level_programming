2b3dc4dd4cf7b54eab9025a2a67ec177bf225246
root <dorian.oufer@gmail.com>
2025-06-18 13:15:25 +0000

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -14,9 +14,9 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 16; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-	putchar (0 + num);
+	putchar ('0' + num);
 	}
 
 	for (letter = 'a'; letter <= 'j'; letter++)
