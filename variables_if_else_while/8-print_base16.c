75dbaa867cef9286287d7a20d5b270ff5c7e9412
root <dorian.oufer@gmail.com>
2025-06-18 12:33:46 +0000

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: prints all the numbers of base 16 in lowercase, followed by a new line.
+ * Description: prints all the numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
  */
