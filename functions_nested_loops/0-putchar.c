b5321a9b88729229c7da92eed2677c5775c02ba9
root <dorian.oufer@gmail.com>
2025-06-20 09:40:40 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,22 +2,25 @@
 
 /**
  * main - Entry point
- *
+ *@c : Character
  * Description: Prints "_putchar" followed by a new line
+ * using the custom _putchar function.
  *
  * Return: Always 0 (Success)
  */
+
+int _putchar (char c);
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
