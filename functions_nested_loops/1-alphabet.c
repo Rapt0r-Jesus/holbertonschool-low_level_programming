a10dd1dd4f9bfee741667a5ae0ef574418ff3443
root <dorian.oufer@gmail.com>
2025-06-20 10:30:55 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include "main.h"
 
 void print_alphabet(void)
 {
-	for (char c = 'a'; c <= 'z'; c++);
-	{ 
-		printf("%c", c);
-	}
-	printf("\n");
-}
-	return(0);
+    char c = 'a';
+
+    while (c <= 'z')
+    {
+        _putchar(c);
+        c++;
+    }
+
+    _putchar('\n');
 
