951b38a7609794af501cee248a809f08d8e6eb52
root <dorian.oufer@gmail.com>
2025-06-22 13:15:54 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_last_digit - Affiche le dernier chiffre d'un entier
+ * @x: L'entier dont on veut afficher le dernier chiffre
+ *
+ * Return: Le dernier chiffre de l'entier
+ */
+
 int print_last_digit(int x)
 
 {
