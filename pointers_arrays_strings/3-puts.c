83162c583e5fff11662b3b4febe479939ff1a45a
root <dorian.oufer@gmail.com>
2025-06-25 13:50:22 +0000

    3-puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Affiche une chaîne de caractères suivie d'une nouvelle ligne
+ * @str: Pointeur vers la chaîne de caractères à afficher
+ *
+ * Cette fonction utilise puts pour afficher la chaîne
+ * passée en paramètre, suivie d'un retour à la ligne.
+ */
+
+void _puts(char *str)
+
+{
+	puts(str);
+}
