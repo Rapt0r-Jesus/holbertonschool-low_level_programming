54c27bd00c3666f3ea701c6769ba9412d2ebebd4
root <dorian.oufer@gmail.com>
2025-06-25 11:40:28 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an int to 98 via its pointer.
+ * @n: Pointer to an int
+ */
+
+int main(void);
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+#include <stdlib.h>
+
+void reset_to_98(int *n);
+#endif
