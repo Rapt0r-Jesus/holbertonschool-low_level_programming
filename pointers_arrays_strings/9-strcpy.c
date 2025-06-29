8b1a90e360c3a8e256ac50f16841009d03e3fc54
root <dorian.oufer@gmail.com>
2025-06-29 12:02:00 +0000

    9-strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - Copies a string including the null terminator
+ * @dest: Destination buffer
+ * @src: Source string
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
