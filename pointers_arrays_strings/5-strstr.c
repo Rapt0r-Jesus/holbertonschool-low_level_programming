d33287b9a94ee23a3d003e4ecd0652ce3f9c38c1
root <dorian.oufer@gmail.com>
2025-07-01 09:16:57 +0000

    5-strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	 for (i = 0; (haystack[i]); i++)
+        {
+                for (j = 0; (needle[j]); j++)
+                {
+			if (haystack[i + j] != needle[j])
+				break;
+	}
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
+
+return(0);
+}
