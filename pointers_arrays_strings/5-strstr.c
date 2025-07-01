06906be7e76d9736230d30b695060c2ff5058b64
root <dorian.oufer@gmail.com>
2025-07-01 12:07:34 +0000

    7-print_chessboard

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,17 +12,18 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j; 
+	int i, j;
+
 	for (i = 0; (haystack[i]); i++)
-        {
-	for (j = 0; (needle[j]); j++)
-                {
+	{
+		for (j = 0; (needle[j]); j++)
+		{
 			if (haystack[i + j] != needle[j])
 				break;
-	}
-			if (needle[j] == '\0')
-				return (&haystack[i]);
 		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
 
-return(0);
+return (0);
 }
