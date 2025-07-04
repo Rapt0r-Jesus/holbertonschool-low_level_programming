1b2c26ca916ac42b11cd4aacf4a64cbde852c077
root <dorian.oufer@gmail.com>
2025-07-04 11:28:09 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+        (void)argc;
+        printf("%d\n", argv[0]);
+        return (0);
+}
