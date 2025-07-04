657f86b80d8840cc8026acad32cebb6358a4ee20
root <dorian.oufer@gmail.com>
2025-07-04 07:38:19 +0000

    1-print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,12 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-}
-else
-{
-_print_rev_recursion(s + 1);
-}
+	if (*s == '\0')
+	{
+		 _putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+	}
 	_putchar (*s);
 }
