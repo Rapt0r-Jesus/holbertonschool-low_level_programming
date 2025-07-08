1cbdbfe22d765872a766fff831d4b7c4f2beb5e1
root <dorian.oufer@gmail.Com>
2025-07-08 08:01:08 +0000

    2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,36 +12,36 @@
 
 char *str_concat(char *s1, char *s2)
 {
- unsigned int i, j;
- unsigned int lenght1 = 0
- unsigned int lenght2 = 0
-	char buffer;
+unsigned int i, j;
+unsigned int lenght1 = 0;
+unsigned int lenght2 = 0;
+char *buffer;
 
 if (s1 == NULL)
 {
-	return("");
+	return ("");
 }
 if (s2 == NULL)
 {
-	return("");
+	return ("");
 }
-        while (s1[lenght1] != '\0')
+	while (s1[lenght1] != '\0')
 	lenght1++;
-	
+
 	while (s2[lenght2] != '\0')
 	lenght2++;
 
-        buffer = malloc (lenght1 + lenght2 + 1);
-        if (buffer == NULL)
-                return (NULL);
+	buffer = malloc(lenght1 + lenght2 + 1);
+	if (buffer == NULL)
+	return (NULL);
 
-        for (i = 0; i < lenght1; i++)
-                buffer[i] = s1[i];
+	for (i = 0; i < lenght1; i++)
+	buffer[i] = s1[i];
 
 	for (j = 0; j < lenght2; j++)
-                buffer[lenght1 + j] = s2[j];
+	buffer[lenght1 + j] = s2[j];
 
-        buffer[lenght1 + lenght2] = '\0';
+	buffer[lenght1 + lenght2] = '\0';
 
-        return (buffer);
+	return (buffer);
 	}
