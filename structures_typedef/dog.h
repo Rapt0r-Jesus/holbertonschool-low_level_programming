db908dc0f1420318d952338261135dbffca7eac2
root <dorian.oufer@gmail.Com>
2025-07-11 10:33:46 +0000

    new dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 
 /**
@@ -15,5 +18,9 @@ struct dog {
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif
