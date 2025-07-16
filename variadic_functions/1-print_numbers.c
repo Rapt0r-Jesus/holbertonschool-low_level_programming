f443583fa18871d1472b891e5ebf0f1faeb4cb7d
root <dorian.oufer@gmail.com>
2025-07-16 13:31:59 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,10 +22,12 @@ va_start(args, n);
 for (i = 0; i < n; i++)
 {
 	printf("%d", va_arg(args, int));
-}
+	
 if (separator != NULL && i < n - 1)
 {
 	printf("%s", separator);
 }
+printf("\n");
 va_end(args);
+}
 }
