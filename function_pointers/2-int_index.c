142d45f40bb2f31bfbc67c30f1ffc0e0c4ea6f8d
root <dorian.oufer@gmail.com>
2025-07-16 09:04:55 +0000

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    if (array == NULL || cmp == NULL || size <= 0)
+        return -1;
 
+    for (int i = 0; i < size; i++)
+    {
+        if (cmp(array[i]) != 0)
+            return i;
+    }
+
+    return -1;
+}
