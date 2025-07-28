4c055e47d81586202150952dcde39ae65721f0e6
root <dorian.oufer@gmail.com>
2025-07-28 09:27:56 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_list - Affiche tous les éléments d'une liste list_t
+ * @h: pointeur vers le premier nœud de la liste
+ *
+ * Return: nombre total de nœuds dans la liste
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+
+        h = h->next;
+        count++;
+    }
+
+    return (count);
+}
